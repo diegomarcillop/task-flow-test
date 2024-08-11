a69f39af17deaa1160c225a97b05022e7160237f
Diego Marcillo <dfmarcillop@gmail.com>
2024-08-11 02:22:40 -0500

    remove color style

--- src/components/Task/TaskStates.tsx
+++ src/components/Task/TaskStates.tsx
@@ -23,7 +23,7 @@ export const TaskStates = () => {
       {states.map((state, index) => (
         <button
           key={index}
-          className={`font-light text-slate-800 rounded-2xl bg-orange-300 px-6 text-sm h-8 ${state.color}`}
+          className={`font-light text-slate-800 rounded-2xl px-6 text-sm h-8 ${state.color}`}
         >
           {state.name} <span className="font-bold">({tasks.filter((e: any) => e.state === state.key).length})</span>
         </button>
